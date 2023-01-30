1b6c8b803234e5dc6a45359467e5ebd9a66a0724
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-30 21:33:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,33 @@
+# Configure the AWS provider
 provider "aws" {
-  region  = var.AWS_REGION # value will be taken form vars.tf file.
+  region = "eu-west-1"
 }
 
-variable "AWS_REGION" {
-  default = "ap-south-1"
-}
-
-variable "AMIS" {
-  type = map(string)
-  default = {
-    "ap-south-1" = "ami-0851b76e8b1bce90b" # ami which will be used to launch                           instance.
+# Create a Security Group for an EC2 instance 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port	  = "${var.server_port}"
+    to_port	    = "${var.server_port}"
+    protocol	  = "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
   }
 }
 
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"  # username which will be used while doing remote-execution with the launched instance.
-}
-
-resource "tls_private_key" "oskey" {
-  algorithm = "RSA"
-}
-
-resource "local_file" "myterrakey" {
-  content  = tls_private_key.oskey.private_key_pem
-  filename = "myterrakey.pem"
-}
-
-resource "aws_key_pair" "key121" {
-  key_name   = "myterrakey"
-  public_key = tls_private_key.oskey.public_key_openssh
-}
-
-resource "aws_instance" "mytfinstance" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.key121.key_name
-
-  provisioner "file" {
-    source      = "script.sh"
-    destination = "/tmp/script.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo /tmp/script.sh"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = var.INSTANCE_USERNAME
-    private_key = tls_private_key.oskey.private_key_pem
-    host        = aws_instance.mytfinstance.public_ip
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami			                = "ami-785db401"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
+  
+  user_data = <<-EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p "${var.server_port}" &
+	      EOF
+			  
+  tags {
+    Name = "terraform-example"
   }
 }
