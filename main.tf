72a05647c191a8bc84289a3265ddf9141f8b230e
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-30 21:03:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,83 +1,93 @@
-variable "awsprops" {
- 
-    default = {
-    region = "us-east-1"
-    vpc = "vpc-5234832d"
-    ami = "ami-0c1bea58988a989155"
-    itype = "t2.micro"
-    subnet = "subnet-81896c8e"
-    publicip = true
-    keyname = "myseckey"
-    secgroupname = "IAC-Sec-Group"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
   }
-}
 
+}
 provider "aws" {
-  region = lookup(var.awsprops, "region")
+  region = var.region
 }
-
-resource "aws_security_group" "project-iac-sg" {
-  name = lookup(var.awsprops, "secgroupname")
-  description = lookup(var.awsprops, "secgroupname")
-  vpc_id = lookup(var.awsprops, "vpc")
-
-  // To Allow SSH Transport
-  ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // To Allow Port 80 Transport
-  ingress {
-    from_port = 80
-    protocol = ""
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
-resource "aws_instance" "project-iac" {
-  ami = lookup(var.awsprops, "ami")
-  instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
-  associate_public_ip_address = lookup(var.awsprops, "publicip")
-  key_name = lookup(var.awsprops, "keyname")
-
-
-  vpc_security_group_ids = [
-    aws_security_group.project-iac-sg.id
-  ]
-  root_block_device {
-    delete_on_termination = true
-    iops = 150
-    volume_size = 50
-    volume_type = "gp2"
+resource "aws_instance" "servernode" {
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
   }
   tags = {
-    Name ="SERVER01"
-    Environment = "DEV"
-    OS = "UBUNTU"
-    Managed = "IAC"
+    "name" = "DeployVM"
   }
-
-  depends_on = [ aws_security_group.project-iac-sg ]
+}
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "ECR-LOGIN-AUTO"
+}
+resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+output "instance_public_ip" {
+  value     = aws_instance.servernode.public_ip
+  sensitive = true
 }
 
-
-output "ec2instance" {
-  value = aws_instance.project-iac.public_ip
+variable "region" {
+    default = "us-east-1"
+}
+variable "public_key" {
+  
+}
+variable "private_key" {
+  
+}
+variable "key_name" {
+  
 }
