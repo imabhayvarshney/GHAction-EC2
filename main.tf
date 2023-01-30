7469211dc46659c80a98b5b832b1c61b01969b87
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-30 21:25:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,93 +1,58 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.0"
-    }
+provider "aws" {
+  profile = "samarth"
+  region  = var.AWS_REGION # value will be taken form vars.tf file.
+}
+
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    "ap-south-1" = "ami-0851b76e8b1bce90b" # ami which will be used to launch                           instance.
+  }
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"  # username which will be used while doing remote-execution with the launched instance.
+}
+
+resource "tls_private_key" "oskey" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "myterrakey" {
+  content  = tls_private_key.oskey.private_key_pem
+  filename = "myterrakey.pem"
+}
+
+resource "aws_key_pair" "key121" {
+  key_name   = "myterrakey"
+  public_key = tls_private_key.oskey.public_key_openssh
+}
+
+resource "aws_instance" "mytfinstance" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key121.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
   }
 
-}
-provider "aws" {
-  region = var.region
-}
-resource "aws_instance" "servernode" {
-  ami                    = "ami-052efd3df9dad4825"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
   connection {
     type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = var.private_key
-    timeout     = "4m"
+    user        = var.INSTANCE_USERNAME
+    private_key = tls_private_key.oskey.private_key_pem
+    host        = aws_instance.mytfinstance.public_ip
   }
-  tags = {
-    "name" = "DeployVM"
-  }
-}
-resource "aws_iam_instance_profile" "ec2-profile" {
-  name = "ec2-profile"
-  role = "ECR-LOGIN-AUTO"
-}
-resource "aws_security_group" "maingroup" {
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 80
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 80
-    }
-  ]
-}
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = var.public_key
-}
-output "instance_public_ip" {
-  value     = aws_instance.servernode.public_ip
-  sensitive = true
-}
-
-variable "region" {
-    default = "us-east-1"
-}
-variable "public_key" {
-  
-}
-variable "private_key" {
-  
-}
-variable "key_name" {
-  
 }
