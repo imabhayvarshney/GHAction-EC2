0d0782ce6bb2057a3e1d4033b6d022bc76446bbb
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-31 12:37:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ provider "aws" {
 }
 
 # Create an EC2 instance
-resource "aws_instance" "example" {
+resource "aws_instance" "Control" {
+  ami		= "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+
+  tags =  { 
+	  Name = "Terraform Ec2"
+  }
+	
+resource "aws_instance" "worker" {
   ami		= "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
 
