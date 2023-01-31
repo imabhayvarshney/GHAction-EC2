55df61e1297a79f5865e0f631a3e498775d34a95
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-31 09:10:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,19 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
-}
-
-# Create a Security Group for an EC2 instance 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  
-  ingress {
-    from_port	  = "${var.server_port}"
-    to_port	    = "${var.server_port}"
-    protocol	  = "tcp"
-    cidr_blocks	= ["0.0.0.0/0"]
-  }
+  region = "us-east-1"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami			                = "ami-785db401"
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
-  
-  user_data = <<-EOF
-	      #!/bin/bash
-	      echo "Hello, World" > index.html
-	      nohup busybox httpd -f -p "${var.server_port}" &
-	      EOF
-			  
+  ami		= "ami-785db401"
+  instance_type = "t2.micro"
+
+  tags =  { 
+	  Name = "Terraform Ec2"
+  }
+	
+outputs " my console output" {
+	value = aws_instance.example.public_ip
+}
   
 }
