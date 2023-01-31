04874098648192c8859014cc94a637391d8217de
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-31 12:37:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 # Output variable: Public IP address
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_instance.Control.public_ip}"
+}
+
+output "public_ip" {
+  value = "${aws_instance.worker.public_ip}"
 }
